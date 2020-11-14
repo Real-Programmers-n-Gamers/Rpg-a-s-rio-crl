b9c0029f9d31128e3d3c669b54cb84ec9b794f8f
Vitor <40408619@live.napier.ac.uk>
2020-11-14 22:22:22 +0000

    moveto
    camera control
    character controls

---
+++ Assets/CameraBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 temp= Vector2.MoveTowards(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position, speed * Time.deltaTime);
+        temp.z = transform.position.z;
+        transform.position = temp;
+    }
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class Controller : MonoBehaviour
+{
+    public float speed;
+    private Vector2 moveVector;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.gravityScale = 0;
+        rb2d.drag = 5;
+        rb2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb2d.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVector = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime,Input.GetAxis("Vertical")*speed*Time.deltaTime) ;
+
+        if (moveVector.magnitude != 0) rb2d.AddForce(moveVector);
+    }
+}
