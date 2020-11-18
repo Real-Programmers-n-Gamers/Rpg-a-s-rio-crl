d3cbd504d931ad0ca2f071ec73961f9881afdfd5
Tugueto <joao_amoedo@gmx.com>
2020-11-18 00:26:07 +0000

    created some scrips for enums
    enums de alguns items
    enums de stats do character

---
+++ Assets/Character/ItemList.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ItemList
+{
+public enum itemList
+{
+SmallHPPotion, MediumHPPotion, LargeHPPotion
+}
+
+}   
+
+
---
+++ Assets/Character/Stats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Stats
+{
+  public enum StatTypes
+  {
+      Strength,Inteligence,Speed,Dexterity,HP,AP,MP,XP
+  }
+public static int str {get; private set;}
+public static int intel {get; private set;}
+public static int spd {get; private set;}
+public static int dex {get; private set;}
+public static int hp {get; private set;}
+public static int ap {get; private set;}
+public static int mp {get; private set;}
+public static int xp {get; private set;}
+public enum Alignment
+{
+ Death,Life
+}
+
+
+}
+
